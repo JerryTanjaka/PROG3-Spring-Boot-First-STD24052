9fc0629f6b8167eeff0789b1cd8f69ebab36ccf9
TANJAKA Jerry Jacques <tanjakajerryjacques@gmail.com>
2026-03-13 09:05:03 +0300

    init project

---
+++ src/main/java/hei/td1spingpriseenmain/Td1SpingPriseEnMainApplication.java
@@ -0,0 +1,13 @@
+package hei.td1spingpriseenmain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Td1SpingPriseEnMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Td1SpingPriseEnMainApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hei/td1spingpriseenmain/Td1SpingPriseEnMainApplicationTests.java
@@ -0,0 +1,13 @@
+package hei.td1spingpriseenmain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Td1SpingPriseEnMainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
